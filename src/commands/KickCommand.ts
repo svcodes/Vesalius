fead917d2ea836e4c01a2bb410e0f2feb43b26b4
svcodes <61701181+svcodes@users.noreply.github.com>
2021-02-18 16:43:31 -0600

    make a kick command
    idk if this actually works

--- src/commands/KickCommand.ts
+++ src/commands/KickCommand.ts
@@ -1,1 +1,33 @@
+import { Command } from 'discord-akairo';
+import { Message, MessageEmbed } from 'discord.js';
+
+export default class KickCommand extends Command {
+    constructor() {
+        super('kick', {
+            aliases: ['kick','yeet'],
+            args: [
+                {
+                    id: 'person',
+                    type: 'member',
+                },
+                {
+                  id: 'reason',
+                  match: 'rest',
+                  type: 'string'
+                }
+            ],
+          userPermissions: ['KICK_MEMBERS']
+        });
+    }
+
+    exec(message: Message, { person, reason }: { person: GuildMember, reason: string }) {
+        if (message.member!.roles.highest.position <= target.roles.highest.position)
+            return message.channel.send("You aren't allowed to kick this person!");
+        if (!person) return message.channel.send("I can't find this user, or they aren't in the server")
+        if (!person.kickable) return message.channel.send("I can't kick this user!")
+        if (person == message.author) return message.channel.send("You can't kick yourself!")
+        person.kick({reason: reason})
+        return message.channel.send(`Successfully kicked {target.displayName}#{target.discriminator}!`);
+    }
+}
 
