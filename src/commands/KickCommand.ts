1355d91860ed9fd0a94b14bfb4c108b0034efae9
svcodes <61701181+svcodes@users.noreply.github.com>
2021-02-18 16:44:02 -0600

    Update KickCommand.ts

--- src/commands/KickCommand.ts
+++ src/commands/KickCommand.ts
@@ -1,5 +1,5 @@
 import { Command } from 'discord-akairo';
-import { Message, MessageEmbed } from 'discord.js';
+import { Message, GuildMember } from 'discord.js';
 
 export default class KickCommand extends Command {
     constructor() {
